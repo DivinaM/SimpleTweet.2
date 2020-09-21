c904c7e83114e714975e97c1618b6b1747707d8b
DivinaM <divina.mcgowan@gmail.com>
2020-09-21 18:43:25 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,28 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+    public String name;
+    public String ScreenName;
+    public String profileImageUrl;
+    
+
+
+    public static User fromJSON (JSONObject jsonObject) throws JSONException {
+
+        User user = new User();
+        user.name= jsonObject.getString("name");
+        user.ScreenName= jsonObject.getString("screen_name");
+        user.profileImageUrl = jsonObject.getString("profile_image_url_https");
+
+
+        return user;
+
+
+    }
+}
+
+
+
